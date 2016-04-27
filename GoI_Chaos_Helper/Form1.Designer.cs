655aaa36716bb244e042d28234aaf78f66225edb
defrost256 <danko.barna@gmail.com>
2016-04-27 20:33:19 +0200

    InitialCommit

---
+++ GoI_Chaos_Helper/Form1.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace GoI_Chaos_Helper
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cropToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.leftSwapPicture = new System.Windows.Forms.PictureBox();
+            this.arrowPicture = new System.Windows.Forms.PictureBox();
+            this.rightSwapPicture = new System.Windows.Forms.PictureBox();
+            this.nextButton = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.contTab = new System.Windows.Forms.TabPage();
+            this.contImage = new System.Windows.Forms.PictureBox();
+            this.finalTab = new System.Windows.Forms.TabPage();
+            this.finalImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip.SuspendLayout();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSwapPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrowPicture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightSwapPicture)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.contTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.contImage)).BeginInit();
+            this.finalTab.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.finalImage)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.helpToolStripMenuItem});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(971, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.cropToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.newToolStripMenuItem.Text = "New";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.ShowOpenFile);
+            // 
+            // cropToolStripMenuItem
+            // 
+            this.cropToolStripMenuItem.Name = "cropToolStripMenuItem";
+            this.cropToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.cropToolStripMenuItem.Text = "Crop";
+            // 
+            // quitToolStripMenuItem
+            // 
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Size = new System.Drawing.Size(100, 22);
+            this.quitToolStripMenuItem.Text = "Quit";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.QuitApp);
+            // 
+            // helpToolStripMenuItem
+            // 
+            this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Text = "Help";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Filter = "JPG|*.jpg|All files|*.*";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.leftSwapPicture);
+            this.panel1.Controls.Add(this.arrowPicture);
+            this.panel1.Controls.Add(this.rightSwapPicture);
+            this.panel1.Controls.Add(this.nextButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 24);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(971, 32);
+            this.panel1.TabIndex = 1;
+            // 
+            // leftSwapPicture
+            // 
+            this.leftSwapPicture.Dock = System.Windows.Forms.DockStyle.Right;
+            this.leftSwapPicture.Location = new System.Drawing.Point(381, 0);
+            this.leftSwapPicture.Name = "leftSwapPicture";
+            this.leftSwapPicture.Size = new System.Drawing.Size(247, 32);
+            this.leftSwapPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.leftSwapPicture.TabIndex = 3;
+            this.leftSwapPicture.TabStop = false;
+            // 
+            // arrowPicture
+            // 
+            this.arrowPicture.Dock = System.Windows.Forms.DockStyle.Right;
+            this.arrowPicture.Image = global::GoI_Chaos_Helper.Properties.Resources.Arrow;
+            this.arrowPicture.Location = new System.Drawing.Point(628, 0);
+            this.arrowPicture.Name = "arrowPicture";
+            this.arrowPicture.Size = new System.Drawing.Size(96, 32);
+            this.arrowPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.arrowPicture.TabIndex = 2;
+            this.arrowPicture.TabStop = false;
+            // 
+            // rightSwapPicture
+            // 
+            this.rightSwapPicture.Dock = System.Windows.Forms.DockStyle.Right;
+            this.rightSwapPicture.Location = new System.Drawing.Point(724, 0);
+            this.rightSwapPicture.Name = "rightSwapPicture";
+            this.rightSwapPicture.Size = new System.Drawing.Size(247, 32);
+            this.rightSwapPicture.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.rightSwapPicture.TabIndex = 1;
+            this.rightSwapPicture.TabStop = false;
+            // 
+            // nextButton
+            // 
+            this.nextButton.AutoSize = true;
+            this.nextButton.Dock = System.Windows.Forms.DockStyle.Left;
+            this.nextButton.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nextButton.Location = new System.Drawing.Point(0, 0);
+            this.nextButton.Name = "nextButton";
+            this.nextButton.Size = new System.Drawing.Size(75, 32);
+            this.nextButton.TabIndex = 0;
+            this.nextButton.Text = "Next";
+            this.nextButton.UseVisualStyleBackColor = true;
+            this.nextButton.Click += new System.EventHandler(this.nextButton_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Alignment = System.Windows.Forms.TabAlignment.Left;
+            this.tabControl1.Controls.Add(this.contTab);
+            this.tabControl1.Controls.Add(this.finalTab);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tabControl1.Location = new System.Drawing.Point(0, 56);
+            this.tabControl1.Multiline = true;
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(971, 374);
+            this.tabControl1.TabIndex = 2;
+            // 
+            // contTab
+            // 
+            this.contTab.Controls.Add(this.contImage);
+            this.contTab.Location = new System.Drawing.Point(26, 4);
+            this.contTab.Name = "contTab";
+            this.contTab.Padding = new System.Windows.Forms.Padding(3);
+            this.contTab.Size = new System.Drawing.Size(941, 366);
+            this.contTab.TabIndex = 0;
+            this.contTab.Text = "Continuous";
+            this.contTab.UseVisualStyleBackColor = true;
+            // 
+            // contImage
+            // 
+            this.contImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.contImage.Location = new System.Drawing.Point(3, 3);
+            this.contImage.Name = "contImage";
+            this.contImage.Size = new System.Drawing.Size(935, 360);
+            this.contImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.contImage.TabIndex = 0;
+            this.contImage.TabStop = false;
+            // 
+            // finalTab
+            // 
+            this.finalTab.Controls.Add(this.finalImage);
+            this.finalTab.Location = new System.Drawing.Point(26, 4);
+            this.finalTab.Name = "finalTab";
+            this.finalTab.Padding = new System.Windows.Forms.Padding(3);
+            this.finalTab.Size = new System.Drawing.Size(941, 366);
+            this.finalTab.TabIndex = 1;
+            this.finalTab.Text = "Final";
+            this.finalTab.UseVisualStyleBackColor = true;
+            // 
+            // finalImage
+            // 
+            this.finalImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.finalImage.Location = new System.Drawing.Point(3, 3);
+            this.finalImage.Name = "finalImage";
+            this.finalImage.Size = new System.Drawing.Size(935, 360);
+            this.finalImage.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.finalImage.TabIndex = 0;
+            this.finalImage.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(971, 430);
+            this.Controls.Add(this.tabControl1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.leftSwapPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.arrowPicture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.rightSwapPicture)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.contTab.ResumeLayout(false);
+            this.contTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.contImage)).EndInit();
+            this.finalTab.ResumeLayout(false);
+            this.finalTab.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.finalImage)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem helpToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.ToolStripMenuItem cropToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox leftSwapPicture;
+        private System.Windows.Forms.PictureBox arrowPicture;
+        private System.Windows.Forms.PictureBox rightSwapPicture;
+        private System.Windows.Forms.Button nextButton;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage contTab;
+        private System.Windows.Forms.TabPage finalTab;
+        private System.Windows.Forms.PictureBox contImage;
+        private System.Windows.Forms.PictureBox finalImage;
+    }
+}
+
---
+++ GoI_Chaos_Helper/Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Imaging;
+
+namespace GoI_Chaos_Helper
+{
+    public partial class Form1 : Form
+    {
+        Color redBGmin = Color.FromArgb(110, 50, 50), redBGmax = Color.FromArgb(123, 57, 57);
+        Color redFGmin = Color.FromArgb(138, 85, 85), redFGmax = Color.FromArgb(148, 102, 102);
+        Color blueBGmin = Color.FromArgb(39, 77, 95), blueBGmax = Color.FromArgb(50, 82, 106);
+        Color blueFGmin = Color.FromArgb(72, 105, 125), blueFGmax = Color.FromArgb(105, 122, 140);
+
+        enum SearchState { FindRed, EndRed, FindBlue, EndBlue, Finished };
+
+        Pen rectPen = new Pen(Color.Black, 2);
+        Pen fillPen = new Pen(Color.FromArgb(50, 0, 0, 0), 1);
+        Bitmap final, cont;
+        List<Rectangle> rects;
+        List<Bitmap> names = new List<Bitmap>();
+        List<int> currentPosOfName = new List<int>(), currentNameInPos = new List<int>();
+        List<int> finalPosOfName = new List<int>(), finalNameInPos = new List<int>();      //If finalPos[i] == j -> the position of names[i] is rects[j] ; if finalName[i] == j -> the name that goes to rects[i] is names[j] ; Concludes if finalPos[j] == i -> finalName[i] == j
+        Random rng;
+        int currentPos = 0;
+
+         
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void ShowOpenFile(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                rng = new Random((int)DateTime.Now.Ticks);
+                final = new Bitmap(openFileDialog1.FileName);
+                cont = new Bitmap(openFileDialog1.FileName);
+                contImage.Image = cont;
+                finalImage.Image = final;
+                rects = getGoiRects();
+                filterRects();
+                if (rects.Count % 8 != 0)
+                    throw new ArgumentException("Invalid image file, unable to find players");
+                foreach(Rectangle r in rects)
+                {
+                    names.Add(final.Clone(r, final.PixelFormat));
+                }
+                shuffle(1);
+                makeFinalImage();
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics graphics = e.Graphics;
+            if (rects != null)
+                foreach (Rectangle r in rects)
+                    graphics.DrawRectangle(rectPen, r);
+        }
+
+        private void QuitApp(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        List<Rectangle> getGoiRects()
+        {
+            Size s = new Size(final.Width, final.Height);
+            Color currentColor;
+            SearchState state;
+            List<Point> redPoints = new List<Point>(), bluePoints = new List<Point>();
+            List<Rectangle> rects = new List<Rectangle>();
+            bool lastLineIsEmpty  = true;
+
+            for (int y = 0; y < s.Height; y++)
+            {
+                state = SearchState.FindRed;
+                for (int x = 0; x < s.Width; x++)
+                {
+                    currentColor = final.GetPixel(x, y);
+                    if (isInColorRange(state, currentColor))
+                    {
+                        switch (state)
+                        {
+                            case SearchState.FindRed:
+                                lastLineIsEmpty = false;
+                                redPoints.Add(new Point(x, y));
+                                state = SearchState.EndRed;
+                                break;
+                            case SearchState.EndRed:
+                                redPoints.Add(new Point(x, y));
+                                state = SearchState.FindBlue;
+                                break;
+                            case SearchState.FindBlue:
+                                bluePoints.Add(new Point(x, y));
+                                state = SearchState.EndBlue;
+                                break;
+                            case SearchState.EndBlue:
+                                bluePoints.Add(new Point(x, y));
+                                state = SearchState.Finished;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                }
+                if (!lastLineIsEmpty && state == SearchState.FindRed)
+                {
+                    lastLineIsEmpty = true;
+                    rects.Add(getBoundingRect(redPoints));
+                    rects.Add(getBoundingRect(bluePoints));
+                    redPoints.Clear();
+                    bluePoints.Clear();
+                }
+            }
+
+            return rects;
+        }
+
+        private void nextButton_Click(object sender, EventArgs e)
+        {
+            displayNext();
+        }
+
+        private bool isInColorRange(SearchState state, Color color)
+        {
+            return
+             (state == SearchState.FindRed &&
+             color.R < redFGmax.R && color.G < redFGmax.G && color.B < redFGmax.B &&
+             color.R > redFGmin.R && color.G > redFGmin.G && color.B > redFGmin.B) ||
+             (state == SearchState.EndRed &&
+             color.R < redBGmax.R && color.G < redBGmax.G && color.B < redBGmax.B &&
+             color.R > redBGmin.R && color.G > redBGmin.G && color.B > redBGmin.B) ||
+             (state == SearchState.FindBlue &&
+             color.R < blueFGmax.R && color.G < blueFGmax.G && color.B < blueFGmax.B &&
+             color.R > blueFGmin.R && color.G > blueFGmin.G && color.B > blueFGmin.B) ||
+             (state == SearchState.EndBlue &&
+             color.R < blueBGmax.R && color.G < blueBGmax.G && color.B < blueBGmax.B &&
+             color.R > blueBGmin.R && color.G > blueBGmin.G && color.B > blueBGmin.B);
+        }
+
+        private Rectangle getBoundingRect(List<Point> points)
+        {
+
+            if (points.Count == 0)
+                throw new ArgumentException("List of points is empty.");
+
+            int minX = int.MaxValue;
+            int minY = minX;
+            int maxX = int.MinValue;
+            int maxY = maxX;
+            int X, Y;
+
+            foreach (Point p in points)
+            {
+                X = p.X;
+                Y = p.Y;
+
+                if (X < minX)
+                    minX = X;
+                if (X > maxX)
+                    maxX = X;
+                if (Y < minY)
+                    minY = Y;
+                if (Y > maxY)
+                    maxY = Y;
+            }
+
+            return new Rectangle(minX, minY, maxX - minX, maxY - minY);
+        }
+
+        private void filterRects()
+        {
+            float avg = 0f;
+            foreach (Rectangle r in rects)
+                avg += r.Height;
+            avg /= rects.Count;
+            rects.RemoveAll(r => r.Height < avg);            
+        }
+
+        private void shuffle(int factor)
+        {
+            for(int i = 0; i < rects.Count; i++)
+            {
+                finalPosOfName.Add(i);
+                finalNameInPos.Add(i);
+                currentPosOfName.Add(i);
+                currentNameInPos.Add(i);
+            }
+            while(factor > 0)
+            {
+                for(int i = 0; i < rects.Count; i++)
+                {
+                    int rand = rng.Next(rects.Count);
+                    int tmp = finalPosOfName[rand];
+                    finalPosOfName[rand] = finalPosOfName[i];
+                    finalPosOfName[i] = tmp;
+                    finalNameInPos[finalPosOfName[i]] = i;
+                    finalNameInPos[finalPosOfName[rand]] = rand;
+                }
+                factor--;
+            }
+        }
+
+        private void placeImage(Bitmap srcImg, Bitmap dstImg, Point pos)
+        {
+          for(int y = 0; y < srcImg.Height; y++)
+                for(int x = 0; x < srcImg.Width; x++)
+                    dstImg.SetPixel(x + pos.X, y + pos.Y, srcImg.GetPixel(x, y));
+        }
+
+        private void makeFinalImage()
+        {
+            for (int i = 0; i < finalPosOfName.Count; i++)
+                placeImage(names[finalPosOfName[i]], final, rects[i].Location);
+        }
+
+        private void displayNext()
+        {
+            int name1 = finalNameInPos[currentPos], name2 = currentNameInPos[currentPos];
+            int pos1 = currentPos, pos2 = currentPosOfName[name1];
+            leftSwapPicture.Image = names[name1];
+            rightSwapPicture.Image = names[name2];
+            currentNameInPos[pos1] = name1;
+            currentNameInPos[pos2] = name2;
+            currentPosOfName[name1] = pos1;
+            currentPosOfName[name2] = pos2;
+            placeImage(names[name1], cont, rects[pos1].Location);
+            placeImage(names[name2], cont, rects[pos2].Location);
+        }
+    }
+}
